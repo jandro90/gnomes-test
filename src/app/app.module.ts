ade28e6779ee87015737075f62444efd82261390
Alejandro Gómez <agp901@gmail.com>
2018-12-04 17:08:37 +0100

    added spinner component, routing and home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { SpinnerComponent } from 'src/utils/spinner/spinner.component';
+import { HomeComponent } from 'src/views/home/home.component';
+
+import { routes } from 'src/routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpinnerComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    MDBBootstrapModule.forRoot()
+    MDBBootstrapModule.forRoot(),
+    RouterModule.forRoot(routes),
   ],
   providers: [],
   bootstrap: [AppComponent]
